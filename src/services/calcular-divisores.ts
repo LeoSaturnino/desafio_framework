f0f3f519d77734199f0fd60589ab056d9f96572b
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-24 16:43:25 -0300

    feat: criado controller calcular divisores e validacoes do valor informado

---
+++ src/services/calcular-divisores.ts
@@ -0,0 +1,9 @@
+export class CalcularDivisores {
+  executar(num: any): any {
+    if (!num) return new Error("Valor não informado");
+
+    if (isNaN(num)) return new Error("Valor informado não é um Número");
+
+    return;
+  }
+}
