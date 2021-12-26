e26dc818dfdb3a3b56377f9536fd6b3b19d70103
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-26 20:14:17 -0300

    refactor: função de calular divisores

--- src/services/calcular-divisores.ts
+++ src/services/calcular-divisores.ts
@@ -6,11 +6,13 @@ export class CalcularDivisores {
 
     const divisores = [1];
 
-    for (let i = 2; i <= num; i++) {
+    for (let i = 2; i <= num / 2; i++) {
       if (num % i === 0) {
         divisores.push(i);
       }
     }
+
+    divisores.push(num);
 
     return divisores;
   }
