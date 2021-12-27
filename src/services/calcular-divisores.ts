2a02028e9a7de5aa002058e37b3280da8a709d75
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-27 10:19:14 -0300

    feat: adicionado tratativa para números negativos e para o zero (0)

--- src/services/calcular-divisores.ts
+++ src/services/calcular-divisores.ts
@@ -1,8 +1,15 @@
 export class CalcularDivisores {
   executar(num: any): number[] {
+    if (num === 0)
+      throw new Error(
+        "O zero(0) é divisível por todos os números, menos por ele mesmo"
+      );
+
     if (!num) throw new Error("Valor não informado");
 
     if (isNaN(num)) throw new Error("Valor informado não é um Número");
+
+    if (Math.sign(num) === -1) num = Math.abs(num);
 
     const divisores = [1];
 
