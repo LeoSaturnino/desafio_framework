a880557d1eb3933630de7330f33e047823de7095
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-27 00:14:07 -0300

    feat: criado o servidor usando express

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,18 @@
-import { CalcularDivisores } from "./services/calcular-divisores";
-import { VerificarPrimo } from "./services/verificar-primo";
+import express from "express";
+import { Request, Response } from "express";
 
-const readline = require("readline");
+import { CalcularDivisoresController } from "./controller/calcular-divisores-controller";
 
-const leitor = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });
 
-const perguntar = (pergunta) =>
-  new Promise((resolve) =>
-    leitor.question(pergunta, (resposta) => resolve(resposta))
-  );
+app.get("/calc/:num", (req: Request, res: Response): Response => {
+  const calc = new CalcularDivisoresController();
+  const resp = calc.executar(req);
 
-const executar = async () => {
-  try {
-    const valor = await perguntar("Informe um número: ");
-    console.log();
-    
-    console.log("Números de Entrada: ");
-
-    console.log(valor);
-
-    console.log();
-
-    const calc = new CalcularDivisores();
-    const divisores = calc.executar(valor);
-
-    console.log("Números Divisores: ");
-
-    console.log(divisores);
-
-    console.log();
-
-    const verif = new VerificarPrimo();
-    const divisoresPrimo: number[] = [];
-
-    if (Array.isArray(divisores)) {
-      for (let i = 0; i < divisores.length; i++) {
-        if (verif.executar(divisores[i])) {
-          divisoresPrimo.push(divisores[i]);
-        }
-      }
-
-      console.log("Números Divisores Primo: ");
-
-      console.log(divisoresPrimo);
-
-      console.log();
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-executar();
+  return res.send(resp);
+});
